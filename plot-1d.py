026a86a8681f6863628c583094a27e39cd1dbb95
Alexandre Voute <alexandre.voute@universite-paris-saclay.fr>
2023-03-10 11:05:08 +0100

    Major refactor
    Some variables are given more explicit names
    Naming style according to Google
    Plot is simplified (one subplot, only energies, no weights)
    All degrees of freedom are treated on an equal footing, using flat
    multidimensional indexing

--- plot-1d.py
+++ plot-1d.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 import numpy as np
 from cycler import cycler as cyc
 import sys
-import copy
+from copy import *
 import ConstantsSI as SI
 from dataclasses import dataclass
 
@@ -21,57 +21,43 @@ plt.rc('axes', prop_cycle=line_cycler)
 class PlotProps:
     x_lim_min : float
     x_lim_max : float
-    x_step : float
     x_label : str
     y_lim_min : float
     y_lim_max : float
-    y_step : float
     y_label : float
     legend_labels : str
     title : str
 
-def makeFigure(rc, l_en, r_vlines, l_w, pprops):
-    fig, lax = plt.subplots(2,1,gridspec_kw=dict(height_ratios=[3,1]))
-    lsc = [[None] * 4 ] * 2
+def MakeFigure(x, energies_a, energies_d, pprops):
+    fig, ax = plt.subplots()
+    lines_a = [None] * (len(energies_a))
+    lines_d = [None] * (len(energies_d))
     
-    ax = lax[0]
-    sc = lsc[0]
-    for n, en in enumerate(l_en):
-        sc[n], = ax.plot(rc, en)
+    for n, en in enumerate(energies_a):
+        lines_a[n], = ax.plot(x, en)
+
+    for n, en in enumerate(energies_d):
+        lines_d[n], = ax.plot(x, en)
+
     ax.axhline(linewidth=0.5, color='gray')
-    for rvl in r_vlines:
-        ax.axvline(rvl, linewidth=0.5, color='gray', linestyle='dashed')
+
     ax.grid(True)
     ax.set_xlim(pprops.x_lim_min, pprops.x_lim_max)
     ax.set_ylim(pprops.y_lim_min, pprops.y_lim_max)
     ax.set_ylabel(pprops.y_label)
     ax.set_title(pprops.title)
-    ax.legend(sc, pprops.legend_labels, loc="upper right")
-
-    ax = lax[1]
-    sc = lsc[1]
-    for n, w in enumerate(l_w):
-        sc[n], = ax.plot(rc, w)
-    ax.axhline(linewidth=0.5, color='gray')
-    for rvl in r_vlines:
-        ax.axvline(rvl, linewidth=0.5, color='gray', linestyle='dashed')
-    ax.grid(True)
-    ax.set_xlim(pprops.x_lim_min, pprops.x_lim_max)
-    ax.set_ylim(0, 1)
-    ax.set_xlabel(pprops.x_label)
-    ax.set_ylabel('Weights')
-    ax.legend(sc, pprops.legend_labels, loc="upper right")
+    ax.legend(lines_a, pprops.legend_labels, loc="upper right")
 
     fig.set_size_inches(11.69,8.27)     # A4 paper size
 
-    return fig, lax, lsc
+    return fig, ax, lines_a
 
-def parseChoice(indices_or_x):
+def ParseChoice(indices_or_x):
     assert(indices_or_x.count("x") == 1)
-    choice_list = [[] for l in range(3) ]
+    cut_coord_indices = [[] for _ in range(len(indices_or_x)) ]
     for n,s in enumerate(indices_or_x):
         if s == "x":
-            plot_coord = n
+            axis_dim = n
             continue
         comma_separated = s.split(",")
         for t in comma_separated:
@@ -80,35 +66,73 @@ def parseChoice(indices_or_x):
                 start = int(colon_separated[0])
                 end = int(colon_separated[1])
                 for i in range(start,end+1):
-                    choice_list[n].append(i)
+                    cut_coord_indices[n].append(i)
             else:
-                choice_list[n].append(int(t))
+                cut_coord_indices[n].append(int(t))
         
-    return plot_coord, choice_list
+    return axis_dim, cut_coord_indices
 
-def main():
-    assert(len(sys.argv)==5 or len(sys.argv)==6)
-    filename = sys.argv[1]
-    coord_x, idx = parseChoice(sys.argv[2:5])
-    saveFig = False
-    if sys.argv[-1] == "-s":
-        saveFig = True
+def GetPlotProperties(d):
+    leg_labels = [r"1 $A'$", r"2 $A'$", r"3 $A'$", r"1 $A''$"] 
+    if d == 0:
+        props = PlotProps(
+                x_lim_min = -0.2 ,
+                x_lim_max =  0.4 ,
+                x_label = r"$x$" ,
+                y_lim_min = -2.0 ,
+                y_lim_max =  2.0 ,
+                y_label = r"Energy / eV" ,
+                legend_labels = leg_labels ,
+                title = "",
+                )
+    elif d == 1:
+        props = PlotProps(
+                x_lim_min = -0.5 ,
+                x_lim_max =  1.0 ,
+                x_label = r"$y$" ,
+                y_lim_min = -2.0 ,
+                y_lim_max =  2.0 ,
+                y_label = r"Energy / eV" ,
+                legend_labels = leg_labels ,
+                title = "",
+                )
+    elif d == 2:
+        props = PlotProps(
+                x_lim_min = 0.0 ,
+                x_lim_max =  180.0 ,
+                x_label = r"$\theta$ / ${}^{\circ}$" ,
+                y_lim_min = -2.0 ,
+                y_lim_max =  2.0 ,
+                y_label = r"Energy / eV" ,
+                legend_labels = leg_labels ,
+                title = "",
+                )
+    return props
+
+def main(argv):
+    assert(len(argv)==5 or len(argv)==6)
+    file_name = argv[1]
+    axis_dim, cut_coord_indices = ParseChoice(argv[2:5])
+    save_figure = False
+    if argv[-1] == "-s":
+        save_figure = True
     
     # Read reference energies
     e_ref = np.loadtxt('ref_en.txt')
 
     # Read data
-    data = np.loadtxt(filename, skiprows=1)
-    with open(filename, 'r') as f:
-        strHeader = f.readline()
-    headers = strHeader.split()
+    data = np.loadtxt(file_name, skiprows=1)
+    with open(file_name, 'r') as f:
+        header = f.readline()
+    header_names = header.split()
     
-    ii = data[:,0].astype(int)
-    jj = data[:,1].astype(int)
-    kk = data[:,2].astype(int)
+    indices = data[:,0:3].astype(int)
     coords = data[:,3:6]
-    en_cols = [6,7,8,9]
-    w_cols = [10,11,12,13]
+    dims = 3
+    energies_cols = [6,7,8,9]
+    energies_a = data[:,energies_cols]
+    n_states = len(energies_cols)
+    n_points = len(indices[:,0])
     
     # Transform coordinates
     r_NN_2 = 0.5945
@@ -117,179 +141,84 @@ def main():
     coords[:,0] = (coords[:,0]+0.54725)/1.56814067 - 1
     coords[:,1] = 1 - np.exp(-(coords[:,1]-R0)/R0)
 
-    iu = np.unique(ii)
-    ju = np.unique(jj)
-    ku = np.unique(kk)
-    en = data[:,en_cols]
-    w = data[:,w_cols]
-    Nstates = len(en_cols)
-
     # Common plotting options
-    leg_labels = [r"1 $A'$", r"2 $A'$", r"3 $A'$", r"1 $A''$"] 
-    ap = dict(facecolor='black', arrowstyle="->")
+    coord_names = [r"$r$",r"$R$",r"$\theta$"]
+    coord_units = [r"~\AA{}",r"~\AA{}",r"${}^{\circ}$"]
 
-    ### Plot against the first coordinate
-    if coord_x == 0:
-        r_vlines = [
-                # 1.0341,  # NH in N2H+
-                # 2.822,   # NH in tetratomic N2HRb+
-                ]
+    props = GetPlotProperties(axis_dim)
 
-        props = PlotProps(
-                x_lim_min = -0.2 ,
-                x_lim_max =  0.4 ,
-                x_step = 0.05, 
-                x_label = r"$x$" ,
-                y_lim_min = -2.0 ,
-                y_lim_max =  2.0 ,
-                y_step = 0.5 ,
-                y_label = r"Energy / eV" ,
-                legend_labels = leg_labels ,
-                title = "",
-                )
+    # Diabatization
 
-        for j in idx[1] :
-            for k in idx[2] :
-                select = np.logical_and(jj == j, kk == k)
-                if np.any(select) is False:
-                    continue
-                x = coords[select,0]
-                sort_idx = np.argsort(x)
-                x = x[sort_idx]
-                y = coords[select,1][0]
-                z = coords[select,2][0]
-                l_en = []
-                l_w = []
-                for iz in range(Nstates):
-                    en_i = (en[select,iz]-e_ref)*SI.h2ev
-                    en_i = en_i[sort_idx]
-                    l_en.append(en_i)
-                    w_i = w[select,iz]
-                    w_i = w_i[sort_idx]
-                    l_w.append(w_i)
 
-                title = r"Scan \ce{[Rb + N2H]+} with "\
-                        + r"$y = "      + "{:5.3f}".format(y) + "$, "\
-                        + r"$\theta = " + "{:4.1f}".format(z) + r"{}^{\circ}$, "\
-                        + "DWSA-CASSCF with df=5" + r"\;hartree$^{-1}$"
 
-                props.title = title
-                fig, lax, lsc = makeFigure(x, l_en, r_vlines, l_w, props)
+    # Dimensions of the cut coordinates
+    dims_cut = []
+    for d in range(dims):
+        if d != axis_dim :
+            dims_cut.append(d)
+   
+    # List of all indices where we have a cut
+    # First, count them
+    n_cuts = 1
+    n_cuts_per_dim = [[] for _ in range(dims)]
+    for d in dims_cut:
+        n_cuts_d = len(cut_coord_indices[d])
+        n_cuts_per_dim[d] = n_cuts_d
+        n_cuts *= n_cuts_d
 
-                if saveFig:
-                    plt.savefig("rb-n2-h+_nh_j{:d}k{:d}.png".format(j,k))
-                else:
-                    plt.show()
-                plt.close(fig)
+    # Form the list: each sublist gets an index from each 
+    cuts_list = []
+    for i_cut in range(n_cuts):
+        i_cut_rem = i_cut
+        indices_single_cut = []
+        for d in dims_cut:
+            i_cut_d = i_cut_rem % n_cuts_per_dim[d]
+            i_cut_rem = (i_cut_rem - i_cut_d) // n_cuts_per_dim[d]
+            indices_single_cut.append(cut_coord_indices[d][i_cut_d])
+        cuts_list.append(indices_single_cut)
 
-    ### Plot against the second coordinate
-    if coord_x == 1:
-        r_vlines = [
-                # 1.0341,  # NH in N2H+
-                # 2.822,   # NH in tetratomic N2HRb+
-                ]
+    # For each cut stride, make a plot
+    for indices_cut in cuts_list:
+        select = np.full((n_points,1), True, dtype=bool)
+        for d, i in zip(dims_cut, indices_cut):
+            select = np.logical_and(select, (indices[:,d] == i)[:, np.newaxis])
 
-        props = PlotProps(
-                x_lim_min = -0.5 ,
-                x_lim_max =  1.0 ,
-                x_step = 0.1, 
-                x_label = r"$y$" ,
-                y_lim_min = -2.0 ,
-                y_lim_max =  2.0 ,
-                y_step = 0.5 ,
-                y_label = r"Energy / eV" ,
-                legend_labels = leg_labels ,
-                title = "",
-                )
+        if np.any(select) is False:
+            break
 
-        for i in idx[0] :
-            for k in idx[2] :
-                select = np.logical_and(ii == i, kk == k)
-                if np.any(select) is False:
-                    continue
-                x = coords[select,1]
-                sort_idx = np.argsort(x)
-                x = x[sort_idx]
-                y = coords[select,0][0]
-                z = coords[select,2][0]
-                l_en = []
-                l_w = []
-                for iz in range(Nstates):
-                    en_i = (en[select,iz]-e_ref)*SI.h2ev
-                    en_i = en_i[sort_idx]
-                    l_en.append(en_i)
-                    w_i = w[select,iz]
-                    w_i = w_i[sort_idx]
-                    l_w.append(w_i)
+        x = coords[:,axis_dim][select.flatten()]
+        sort_idx = np.argsort(x)
+        x = x[sort_idx]
+        coords_cut = []
+        for d in dims_cut:
+            coords_cut.append(coords[:,d][select.flatten()][0])
 
-                title = r"Scan \ce{[Rb + N2H]+} with "\
-                        + r"$x = "      + "{:5.3f}".format(y) + r"$, "\
-                        + r"$\theta = " + "{:4.1f}".format(z) + r"{}^{\circ}$, "\
-                        + "DWSA-CASSCF with df=5" + r"\;hartree$^{-1}$"
+        energies_a_cut = []
+        energies_d_cut = []
+        for state in range(n_states):
+            en_i = (energies_a[:,state][select.flatten()]-e_ref)*SI.h2ev
+            en_i = en_i[sort_idx]
+            energies_a_cut.append(en_i)
 
-                props.title = title
-                fig, lax, lsc = makeFigure(x, l_en, r_vlines, l_w, props)
+        title = r"Scan \ce{[Rb + N2H]+} with "
+        for n, d in enumerate(dims_cut):
+            title += coord_names[d] + r" $= " + "{:5.3f}".format(coords_cut[n]) + r"$ " + coord_units[d] + ", "
+        title = title[:-2]
+        props.title = title
 
-                if saveFig:
-                    plt.savefig("rb-n2-h+_grb_i{:d}k{:d}.png".format(i,k))
-                else:
-                    plt.show()
-                plt.close(fig)
+        fig, ax, lines = MakeFigure(x, energies_a_cut, [], props)
 
-    ### Plot against the third coordinate
-    if coord_x == 2:
-        r_vlines = [
-                # 1.0341,  # NH in N2H+
-                # 2.822,   # NH in tetratomic N2HRb+
-                ]
-
-        props = PlotProps(
-                x_lim_min = 0.0 ,
-                x_lim_max =  180.0 ,
-                x_step = 30.0, 
-                x_label = r"$\theta$ / ${}^{\circ}$" ,
-                y_lim_min = -2.0 ,
-                y_lim_max =  2.0 ,
-                y_step = 0.5 ,
-                y_label = r"Energy / eV" ,
-                legend_labels = leg_labels ,
-                title = "",
-                )
-
-        for i in idx[0] :
-            for j in idx[1] :
-                select = np.logical_and(ii == i, jj == j)
-                if np.any(select) is False:
-                    continue
-                x = coords[select,2]
-                sort_idx = np.argsort(x)
-                x = x[sort_idx]
-                y = coords[select,0][0]
-                z = coords[select,1][0]
-                l_en = []
-                l_w = []
-                for iz in range(Nstates):
-                    en_i = (en[select,iz]-e_ref)*SI.h2ev
-                    en_i = en_i[sort_idx]
-                    l_en.append(en_i)
-                    w_i = w[select,iz]
-                    w_i = w_i[sort_idx]
-                    l_w.append(w_i)
-
-                title = r"Scan \ce{[Rb + N2H]+} with "\
-                        + r"$x = "  + "{:5.3f}".format(y) + r"$\;\AA{}, "\
-                        + r"$y = "  + "{:5.3f}".format(z) + r"$\;\AA{}, "\
-                        + "DWSA-CASSCF with df=5" + r"\;hartree$^{-1}$"
-
-                props.title = title
-                fig, lax, lsc = makeFigure(x, l_en, r_vlines, l_w, props)
-
-                if saveFig:
-                    plt.savefig("rb-n2-h+_theta_i{:d}j{:d}.png".format(i,j))
-                else:
-                    plt.show()
-                plt.close(fig)
+        if save_figure:
+            saved_file_name = f"cut_{axis_dim}_"
+            for d, i in zip(dims_cut, indices_cut):
+                saved_file_name += f"{i}{chr(105 + d)}_" 
+            saved_file_name = saved_file_name[:-1]
+            saved_file_name += ".pdf"
+            plt.savefig(saved_file_name)
+        else:
+            plt.show()
+        plt.close(fig)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
