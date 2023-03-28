80168d1467975799ff670408ce7b3c39736ccd0c
Alexandre Voute <alexandre.voute@universite-paris-saclay.fr>
2023-03-28 18:38:38 +0200

    Test SymPolyMat static methods
    New tests for the static methods od SymPolyMat generating zero or
    identity matrices, ex novo or with the same structure as another one,
    are added. Minor fix on eye_like().

--- diabtools.py
+++ diabtools.py
@@ -251,7 +251,7 @@ class SymPolyMat():
         this will be added (with a coefficient 1).
         """
         newmat = SymPolyMat.zero_like(other)
-        for i in range(Ns):
+        for i in range(newmat.Ns):
             for j in range(i+1):
                 if i == j :
                     newmat[i,j][newmat[i,j].zeroPower] = 1
@@ -478,19 +478,56 @@ class TestSymMat:
         assert M[1,0] == M[0,1]
 
     def test_Empty(self):
-        pass
+        Ns = 3
+        Nd = 4
+        M = SymPolyMat(Ns, Nd)
+        assert all([len(m.powers()) == 0 for m in M])
+        assert all([m.Nd == Nd for m in M])
 
     def test_Zero(self):
-        pass
+        Ns = 3
+        Nd = 4
+        M = SymPolyMat.zero(Ns, Nd)
+        assert all([list(m.powers()) == [m.zeroPower] for m in M])
+        assert all([list(m.coeffs()) == [0] for m in M])
+        assert all([m.Nd == Nd for m in M])
 
     def test_ZeroLike(self):
-        pass
+        Ns = 3
+        Nd = 2
+        A = SymPolyMat(Ns,Nd)
+        A[0,0] = NdPoly({(0,1): 2, (3,4): 5})
+        A[1,0] = NdPoly({(5,4): 3, (2,1): 0})
+        A[1,1] = NdPoly({(1,1): 1, (2,2): 2})
+        A[2,0] = NdPoly({(3,3): 3, (4,4): 4})
+        A[2,1] = NdPoly({(3,1): 4, (4,1): 3})
+        A[2,2] = NdPoly({(0,4): 2, (6,6): 6})
+        B = SymPolyMat.zero_like(A)
+        assert all([list(a.powers()) == list(b.powers()) for a,b in zip(A,B)])
+        assert all([list(b.coeffs()) == [0 for _ in range(len(b))] for b in B])
 
     def test_Eye(self):
-        pass
+        Ns = 3
+        Nd = 2
+        I = SymPolyMat.eye(Ns,Nd)
+        assert all([list(i.powers()) == [i.zeroPower] for i in I])
+        assert all([list(I[i,j].coeffs()) == [0] for i in range(Ns) for j in range(i)])
+        assert all([list(I[i,i].coeffs()) == [1] for i in range(Ns)])
 
     def test_EyeLike(self):
-        pass
+        Ns = 3
+        Nd = 2
+        A = SymPolyMat(Ns,Nd)
+        A[0,0] = NdPoly({(0,1): 2, (3,4): 5})
+        A[1,0] = NdPoly({(5,4): 3, (2,1): 0})
+        A[1,1] = NdPoly({(1,1): 1, (2,2): 2})
+        A[2,0] = NdPoly({(3,3): 3, (4,4): 4})
+        A[2,1] = NdPoly({(3,1): 4, (4,1): 3})
+        A[2,2] = NdPoly({(0,4): 2, (6,6): 6})
+        B = SymPolyMat.eye_like(A)
+        assert all([list(B[i,j].powers()) == list(A[i,j].powers()) for i in range(Ns) for j in range(i)])
+        assert all([a_power in B[i,i] for i in range(Ns) for a_power in list(A[i,i].powers())])
+        assert all([B[i,i][B[i,i].zeroPower] == 1 for i in range(Ns)])
      
     def test_Call(self):
         Ns = 5
