42975a0ccfe0ad276d15befaa77fd8e09396ccf0
Alexandre Voute <alexandre.voute@universite-paris-saclay.fr>
2023-03-29 18:51:22 +0200

    Fix symmetry of SymPolyMat.__call__ + test 3s
    When calling SymPolyMat.__call__, the matrix evaluated at a given point
    was not symmetric. This is now fixed.
    A three-state test is added (but not completed) and has allowed to see
    the bug.

--- diabtools.py
+++ diabtools.py
@@ -188,9 +188,7 @@ class SymPolyMat():
         for i in range(self._Ns):   # Compute lower triangular part
             for j in range(i+1):
                 W[:,i,j] = self[i,j](x)
-        for i in range(1, self._Ns):   # Copy to upper triangular off-diagonal part
-            for j in range(i, self._Ns):
-                W[:,i,j] = W[:,j,i]
+                W[:,j,i] = W[:,i,j]
         return W
 
     def __repr__(self): 
@@ -548,6 +546,7 @@ class TestSymMat:
             ))
         Wx = M(data)
         assert Wx.shape == (len(self.testdata)**Nd, Ns, Ns)
+        assert all([np.allclose(Wx[i,:,:], Wx[i,:,:].T) for i in range(len(self.testdata))])
 
 class TestDiabatizer:
     def test_LiF(self, pytestconfig):
@@ -734,6 +733,158 @@ class TestDiabatizer:
         ax3.set_ylabel("$y$")
         plt.show()
 
+    def plot_2d3s_testVSfit(self,X,Y,Wt,Vt,W):
+        import matplotlib.pyplot as plt
+        # Reshape
+        W11_t = Wt[:,0,0].reshape(X.shape)
+        W22_t = Wt[:,1,1].reshape(X.shape)
+        W33_t = Wt[:,2,2].reshape(X.shape)
+        W21_t = np.abs(Wt[:,1,0].reshape(X.shape))
+        W31_t = np.abs(Wt[:,2,0].reshape(X.shape))
+        W32_t = np.abs(Wt[:,2,1].reshape(X.shape))
+        V1_t = Vt[:,0].reshape(X.shape)
+        V2_t = Vt[:,1].reshape(X.shape)
+        V3_t = Vt[:,2].reshape(X.shape)
+
+        W11 = W[:,0,0].reshape(X.shape)
+        W22 = W[:,1,1].reshape(X.shape)
+        W33 = W[:,2,2].reshape(X.shape)
+        W21 = np.abs(W[:,1,0].reshape(X.shape))
+        W31 = np.abs(W[:,2,0].reshape(X.shape))
+        W32 = np.abs(W[:,2,1].reshape(X.shape))
+        V1 = adiabatic(W)[0][:,0].reshape(X.shape)
+        V2 = adiabatic(W)[0][:,1].reshape(X.shape)
+        V3 = adiabatic(W)[0][:,2].reshape(X.shape)
+
+        # Calculate logarithms of abs(differences)
+        dW11 = np.abs(W11-W11_t)
+        dW22 = np.abs(W22-W22_t)
+        dW33 = np.abs(W33-W33_t)
+        dW21 = np.abs(W21-W21_t)
+        dW31 = np.abs(W31-W31_t)
+        dW32 = np.abs(W32-W32_t)
+        dV1 = np.abs(V1-V1_t)
+        dV2 = np.abs(V2-V2_t)
+        dV3 = np.abs(V3-V3_t)
+        dW11[dW11==0] = np.nan
+        dW22[dW22==0] = np.nan
+        dW33[dW33==0] = np.nan
+        dW21[dW21==0] = np.nan
+        dW31[dW31==0] = np.nan
+        dW32[dW32==0] = np.nan
+        dV1[dV1==0] = np.nan
+        dV2[dV2==0] = np.nan
+        dV3[dV3==0] = np.nan
+       
+        ldW11 = np.log10(dW11)
+        ldW22 = np.log10(dW22)
+        ldW33 = np.log10(dW33)
+        ldW21 = np.log10(dW21)
+        ldW31 = np.log10(dW31)
+        ldW32 = np.log10(dW32)
+        ldV1  = np.log10(dV1)
+        ldV2  = np.log10(dV2)
+        ldV3  = np.log10(dV3)
+
+        # Plot
+        cmapW11='Blues_r'
+        cmapW22='Reds_r'
+        cmapW33='Greens_r'
+        cmapW21='bwr'
+        cmapW31='BrBG'
+        cmapW32='PiYG'
+        cmapV1='Purples'
+        cmapV2='plasma'
+        cmapV3='Oranges_r'
+
+        cl = np.linspace(-1,1,21)
+        fig, axs = plt.subplots(3,3)
+        axs[0,0].contour(X,Y,W11_t,levels=cl,cmap=cmapW11)
+        axs[0,0].contour(X,Y,W22_t,levels=cl,cmap=cmapW22)
+        axs[0,0].contour(X,Y,W33_t,levels=cl,cmap=cmapW33)
+        axs[0,1].contour(X,Y,W21_t,levels=21,cmap=cmapW21)
+        axs[0,1].contour(X,Y,W31_t,levels=21,cmap=cmapW31)
+        axs[0,1].contour(X,Y,W32_t,levels=21,cmap=cmapW32)
+        axs[0,2].contour(X,Y,V1_t,levels=cl,cmap=cmapV1)
+        axs[0,2].contour(X,Y,V2_t,levels=cl,cmap=cmapV2)
+        axs[0,2].contour(X,Y,V3_t,levels=cl,cmap=cmapV3)
+
+        axs[1,0].contour(X,Y,W11,levels=cl,cmap=cmapW11)
+        axs[1,0].contour(X,Y,W22,levels=cl,cmap=cmapW22)
+        axs[1,0].contour(X,Y,W33,levels=cl,cmap=cmapW33)
+        axs[1,1].contour(X,Y,W21,levels=21,cmap=cmapW21)
+        axs[1,1].contour(X,Y,W31,levels=21,cmap=cmapW31)
+        axs[1,1].contour(X,Y,W32,levels=21,cmap=cmapW32)
+        axs[1,2].contour(X,Y,V1,levels=cl,cmap=cmapV1)
+        axs[1,2].contour(X,Y,V2,levels=cl,cmap=cmapV2)
+        axs[1,2].contour(X,Y,V3,levels=cl,cmap=cmapV3)
+
+        cs = axs[2,0].contour(X,Y,ldW11,levels=10,cmap=cmapW11)
+        # fig.colorbar(cs, ax=axs[2,0], location='right')
+        cs = axs[2,0].contour(X,Y,ldW22,levels=10,cmap=cmapW22)
+        # fig.colorbar(cs, ax=axs[2,0], location='right')
+        cs = axs[2,0].contour(X,Y,ldW33,levels=10,cmap=cmapW33)
+        # fig.colorbar(cs, ax=axs[2,0], location='right')
+        cs = axs[2,1].contour(X,Y,ldW21,levels=10,cmap=cmapW21)
+        # fig.colorbar(cs, ax=axs[2,1], location='right')
+        cs = axs[2,1].contour(X,Y,ldW31,levels=10,cmap=cmapW31)
+        # fig.colorbar(cs, ax=axs[2,1], location='right')
+        cs = axs[2,1].contour(X,Y,ldW32,levels=10,cmap=cmapW32)
+        # fig.colorbar(cs, ax=axs[2,1], location='right')
+        cs = axs[2,2].contour(X,Y,ldV1,levels=10,cmap=cmapV1)
+        # fig.colorbar(cs, ax=axs[2,2], location='right')
+        cs = axs[2,2].contour(X,Y,ldV2,levels=10,cmap=cmapV2)
+        # fig.colorbar(cs, ax=axs[2,2], location='right')
+
+        axs[0,0].set_ylabel("$y$")
+        axs[1,0].set_ylabel("$y$")
+        axs[2,0].set_xlabel("$x$")
+        axs[2,0].set_ylabel("$y$")
+        axs[2,1].set_xlabel("$x$")
+        axs[2,2].set_xlabel("$x$")
+
+        ax1 = plt.figure().add_subplot(projection='3d')
+        ax1.plot_wireframe(X,Y,W11_t,alpha=0.5,color='darkblue', lw=0.5)
+        ax1.plot_surface(X,Y,W11,alpha=0.5,cmap=cmapW11)
+        ax1.contour(X,Y,W11,alpha=0.5, levels=cl,colors='b')
+        ax1.plot_wireframe(X,Y,W22_t,alpha=0.5,color='darkred', lw=0.5)
+        ax1.plot_surface(X,Y,W22,alpha=0.5,cmap=cmapW22)
+        ax1.contour(X,Y,W22,alpha=0.5, levels=cl,colors='r')
+        ax1.plot_wireframe(X,Y,W33_t,alpha=0.5,color='darkgreen', lw=0.5)
+        ax1.plot_surface(X,Y,W33,alpha=0.5,cmap=cmapW33)
+        ax1.contour(X,Y,W33,alpha=0.5, levels=cl,colors='g')
+        ax1.set_zlim(ax1.get_zlim()[0], cl[-1])
+        ax1.set_xlabel("$x$")
+        ax1.set_ylabel("$y$")
+
+        ax2 = plt.figure().add_subplot(projection='3d')
+        ax2.plot_wireframe(X,Y,W21_t,alpha=0.5,color='k', lw=0.5)
+        ax2.plot_surface(X,Y,W21,alpha=0.5,cmap=cmapW21)
+        ax2.contour(X,Y,W21,alpha=0.5, levels=21, colors='purple')
+        ax2.plot_wireframe(X,Y,W31_t,alpha=0.5,color='k', lw=0.5)
+        ax2.plot_surface(X,Y,W31,alpha=0.5,cmap=cmapW31)
+        ax2.contour(X,Y,W31,alpha=0.5, levels=21, colors='teal')
+        ax2.plot_wireframe(X,Y,W32_t,alpha=0.5,color='k', lw=0.5)
+        ax2.plot_surface(X,Y,W32,alpha=0.5,cmap=cmapW32)
+        ax2.contour(X,Y,W32,alpha=0.5, levels=21, colors='olive')
+        ax2.set_xlabel("$x$")
+        ax2.set_ylabel("$y$")
+
+        ax3 = plt.figure().add_subplot(projection='3d')
+        ax3.plot_wireframe(X,Y,V1_t,alpha=0.5,color='indigo', lw=0.5)
+        ax3.plot_surface(X,Y,V1,alpha=0.5,cmap=cmapV1)
+        ax3.contour(X,Y,V1,alpha=0.5, levels=cl, colors='purple')
+        ax3.plot_wireframe(X,Y,V2_t,alpha=0.5,color='crimson', lw=0.5)
+        ax3.plot_surface(X,Y,V2,alpha=0.5,cmap=cmapV2)
+        ax3.contour(X,Y,V2,alpha=0.5, levels=cl, colors='deeppink')
+        ax3.plot_wireframe(X,Y,V3_t,alpha=0.5,color='darkorange', lw=0.5)
+        ax3.plot_surface(X,Y,V3,alpha=0.5,cmap=cmapV3)
+        ax3.contour(X,Y,V3,alpha=0.5, levels=cl, colors='orange')
+        ax3.set_zlim(ax3.get_zlim()[0], cl[-1])
+        ax3.set_xlabel("$x$")
+        ax3.set_ylabel("$y$")
+        plt.show()
+
     def W_JTCI_2d2s(self):
         dx1 = 1
         dx2 = -1
@@ -771,6 +922,27 @@ class TestDiabatizer:
         })
         W[0,1] = NdPoly({(0,1): 5E-2})
         return W
+
+    def W_ho_2d3s(self):
+        dx1 = -1
+        dx2 = 0
+        dx3 = 1
+        W = SymPolyMat.zero(3,2)
+        W[0,0] = NdPoly({(2,0): 0.1, (0,2): 0.1,})
+        W[1,1] = NdPoly({(0,0): 0.5, (1,0): -0.5})
+        W[2,2] = NdPoly({(2,0): 0.1, (0,2): 0.1,})
+        # W[0,0] = NdPoly({(2,0): 0.0, (0,2): 0.0,})
+        # W[1,1] = NdPoly({(2,0): 0.0, (0,2): 0.0,})
+        # W[2,2] = NdPoly({(2,0): 0.0, (0,2): 0.0,})
+        W[0,0][(0,0)] = W[0,0][(2,0)]*dx1**2 #- W[0,0][(1,0)]*dx1 
+        W[0,0][(1,0)] = -2*W[0,0][(2,0)]*dx1
+        W[2,2][(0,0)] = W[0,0][(2,0)]*dx2**2 + 1#- W[0,0][(1,0)]*dx2 
+        W[2,2][(1,0)] = -2*W[0,0][(2,0)]*dx2
+        W[0,1] = NdPoly({(0,1): 5E-1})
+        W[2,1] = NdPoly({(0,1): 1E-1})
+        W[2,0] = NdPoly({(0,1): 2E-1})
+        return W
+
 
     def test_2d2s_infinity(self, pytestconfig):
         """ Two states, two dimensions.
@@ -829,8 +1001,53 @@ class TestDiabatizer:
             Wx = W(x_data)
             self.plot_2d2s_testVSfit(X,Y,W_test_x,V_t,Wx)
 
-    def test_2d3s(self):
-        pass
+    def test_2d3s(self, pytestconfig):
+        """ Three states, two dimensions.
+        1: Construct an arbitrary diabatic potential matrix W_test
+        2: Diagonalize it in order to get the adiabatic PESs
+        3: Fit a new W_fit to the adiabatic data and check if
+           W_fit == W_test
+        """
+
+        # 0: Prepare coordinates
+        x1, x2, y1, y2 = (-2,2,-1,1)
+        Nx, Ny = (21,21)
+        x = np.linspace(x1,x2,Nx)
+        y = np.linspace(y1,y2,Ny)
+
+        X, Y = np.mgrid[x1:x2:Nx*1j, y1:y2:Ny*1j]
+        x0, y0 = (0,0)
+        X = X - x0
+        Y = Y - y0
+
+        x_data = np.hstack((
+            X.flatten()[:,np.newaxis],
+            Y.flatten()[:,np.newaxis],
+            ))
+
+        # 1: Make target diabatic surfaces
+        W_test = self.W_ho_2d3s()
+        W_test_x = W_test(x_data)
+
+        # 2: Make test adiabatic surfaces
+        V_t = adiabatic(W_test_x)[0]
+
+        # 3: Fit diabatize test adiabatic surfaces
+        # W_guess = SymPolyMat.zero_like(W_test)
+
+        # test2d3s = Diabatizer(2,2,W_guess)
+        # test2d3s.addPoints(x_data, V_t, (0,1))
+        # test2d3s.optimize()
+        # W = test2d3s.Wout
+        # for w, wt in zip(W,W_test):
+        #     for c, ct in zip(w.coeffs(), wt.coeffs()):
+        #         assert abs(c-ct) < 1E-10
+
+        # Show the result if verbose test
+        if pytestconfig.getoption("verbose") > 0:
+            # Wx = W(x_data)
+            # self.plot_2d3s_testVSfit(X,Y,W_test_x,V_t,Wx)
+            self.plot_2d3s_testVSfit(X,Y,W_test_x,V_t,W_test_x)
 
     def test_3d3s(self):
         pass
