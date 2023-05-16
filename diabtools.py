b88daf24199129f9b531fe1048d0b1f171dc46f5
Alexandre Voute <alexandre.voute@universite-paris-saclay.fr>
2023-05-16 16:16:55 +0200

    feat: add 1d switching functions
    Non-class switching functions are added.
    They are not yet integrated in the main code

--- diabtools.py
+++ diabtools.py
@@ -858,6 +858,38 @@ def adiabatic(W):
     """ Return numerical N-states adiabatic potentials from diabatic ones and couplings. """
     return np.linalg.eigh(W)
 
+def switch_poly(x, smin=0, smax=1):
+    """ Polynomial switching function with zero first and second derivatives at 0 and 1.
+    See: Zhang et al., J. Phys. Chem. A, 2004, 108, 41, 8980–8986.
+    https://doi.org/10.1021/jp048339l
+    Beware of the typo: should be -10*x^3 instead of -10*x^2
+    """
+    xs = (x-smin)/(smax-smin)
+    xs = math.max(math.min(xs, 1), 0)
+    # if xs == 0:
+    #     return 1
+    # elif xs == 1:
+    #     return 0
+    # else:
+    #     return 1 - x**3 * (10 + x*(15 - 6*x))
+    return 1 - x**3 * (10 + x*(15 - 6*x))
+
+def switch_sinsin(x, smin=0, smax=1):
+    """ Sine of sine switching function with zero first and second derivative at 0 and 1.
+    See: Sahnoun et al., J. Phys. Chem. A, 2018, 122, 11, 3004–3012.
+    https://doi/10.1021/acs.jpca.8b00150
+    """
+    xs = (x-smin)/(smax-smin)
+    xs = math.max(math.min(xs, 1), 0)
+    # if xs == 0:
+    #     return 1
+    # elif xs == 1:
+    #     return 0
+    # else:
+    #     return np.sin(np.pi/2 * np.sin(np.pi/2 * x) )
+    return (1 + np.sin(np.pi/2*np.sin(np.pi/2*(2*x - 1))))/2
+    
+
 ### MAIN ###
 def main(argv) -> int:
     A = SymPolyMat.eye(2,3)
