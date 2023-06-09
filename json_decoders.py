ad6b20418e8b0e529354b560414dba92df98ec11
Alexandre Voute <alexandre.voute@universite-paris-saclay.fr>
2023-06-09 09:54:50 +0200

    refactor(JSONDecoder): declare one method per type
    Define methods 'parse_<type>' where <type> is the class of the object to
    decode. 'object_hook' calls one of them depending on the key type that
    is read.

--- json_decoders.py
+++ json_decoders.py
@@ -14,26 +14,48 @@ class DiabJSONDecoder(json.JSONDecoder):
     def __init__(self):
         super().__init__(object_hook=self.object_hook)
 
+    def parse_NdPoly(self, dct):
+        P = diabtools.NdPoly.empty(dct["Nd"])
+        P.x0 = dct["x0"]
+        
+        # Get polynomial coefficients
+        for raw_power, coeff in dct["coeffs_by_powers"].items():
+            P[str2tuple(raw_power)] = coeff
+
+        return P
+
+    def parse_SymPolyMat(self, dct):
+        W = diabtools.SymPolyMat(dct["Ns"],dct["Nd"])
+        for ij, poly in dct["elements"].items():
+            W[str2tuple(ij)] = self.parse_NdPoly(poly)
+        
+        return W
+
+    def parse_DampedSymPolyMat(self, dct):
+        # DW = diabtools.DampedSymPolyMat.from_base(
+        #         self.parse_SymPolyMat(dct)
+        #         )
+        # for ij, damp in dct["damping"].items():
+        #     DW.set_damping(str2tuple(ij), self.parse_DampingFunction(damp))
+        return DW
+
     def object_hook(self, dct):
         if "__NdPoly__" in dct:
-            P = diabtools.NdPoly.empty(dct["Nd"])
-            P.x0 = dct["x0"]
-            
-            # Get polynomial coefficients
-            for raw_power, coeff in dct["coeffs_by_powers"].items():
-                P[str2tuple(raw_power)] = coeff
-
-            return P
+            return self.parse_NdPoly(dct)
 
         if "__SymPolyMat__" in dct:
-            W = diabtools.SymPolyMat(dct["Ns"],dct["Nd"])
-            # Recursively parse object representing NdPoly instance
-            for ij, poly in dct["elements"].items():
-                W[str2tuple(ij)] = self.object_hook(poly)
-            
-            return W
+            return self.parse_SymPolyMat(dct)
+        
+        if "__DampedSymPolyMat__" in dct:
+            pass
 
-        else:
-            return dct
+        if "__Diabatizer__" in dct:
+            pass
+
+        if "__Results__" in dct:
+            pass
+
+        # Return the unchanged dict otherwise
+        return dct
 
 
