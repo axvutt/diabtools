c35cd0c95b31e50db16abf352233824c90ee5642
Alexandre Voute <alexandre.voute@universite-paris-saclay.fr>
2023-06-20 18:57:24 +0200

    test: complete wRMSE and wMAE

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -8,13 +8,21 @@ class TestDiagnostics:
     def residuals(self, request):
         return np.linspace(-1,1,request.param)
 
+    @pytest.fixture(scope="class", params=[0,25,50])
+    def delta_residuals(self, request):
+        res = np.zeros(51)
+        k = request.param
+        res[k] = 1
+        return res, k
+
     @pytest.fixture(scope="class", params=[0.1,1,2])
     def constant_weights(self, residuals, request):
         return np.ones(len(residuals))*request.param
 
     @pytest.fixture(scope="class")
-    def heaviside_weights(self, residuals):
-        return np.heaviside(residuals,1)
+    def cos_weights(self, delta_residuals):
+        res, _ = delta_residuals
+        return np.cos(np.linspace(-0.5*np.pi, 0.5*np.pi, len(res)))
 
     def test_rmse(self,residuals):
         n = len(residuals)
@@ -34,31 +42,19 @@ class TestDiagnostics:
     def test_wmae_as_mae(self, residuals, constant_weights):
         assert wMAE(residuals, constant_weights) == pytest.approx(MAE(residuals))
 
-    @pytest.mark.skip
-    def test_wrmse_heaviside(self, residuals, heaviside_weights):
-        n = len(residuals)
-        p = n//2
-        # if n % 2 == 0:
-        #     assert (wRMSE(residuals, heaviside_weights)
-        #         == pytest.approx(np.sqrt(2/3*(4*p-1)/(2*p-1)-1))
-        #     )
-        # else:
-        #     assert (wRMSE(residuals, heaviside_weights)
-        #         == pytest.approx(np.sqrt( 0.5*n/(p+1) * ((2/3*(4*p-1)/(2*p-1) - 1) )))
-        #     )
+    def test_wrmse_deltares(self, delta_residuals, cos_weights):
+        res, k = delta_residuals
+        p = len(res) // 2
+        assert (wRMSE(res, cos_weights)
+            == pytest.approx(np.sqrt(np.cos(0.5*np.pi*(k/p-1))*np.tan(0.25*np.pi/p)))
+        )
 
-    @pytest.mark.skip
-    def test_wmae_heaviside(self, residuals, heaviside_weights):
-        n = len(residuals)
-        p = n//2
-        # if n % 2 == 0:
-        #     assert (wMAE(residuals, heaviside_weights)
-        #         == pytest.approx(p/(2*p-1))
-        #     )
-        # else:
-        #     assert (wMAE(residuals, heaviside_weights)
-        #         == pytest.approx(0.5*n/(p+1) * (p+1)/(2*p+1))
-        #     )
+    def test_wmae_deltares(self, delta_residuals, cos_weights):
+        res, k = delta_residuals
+        p = len(res) // 2
+        assert (wMAE(res, cos_weights)
+            == pytest.approx(np.cos(0.5*np.pi*(k/p-1))*np.tan(0.25*np.pi/p))
+        )
 
 
 
