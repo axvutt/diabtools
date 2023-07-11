fix(Diabatizer): broadcast const domain weights
If a constant weight is set for a domain, broadcast it to an array of
the same size as the number of energy points to fit in that domain.

diabatizer.py
@@ -196,7 +196,7 @@ class Diabatizer:
     def set_domain_weight(self, id_domain, weight):
         """ Assign a fixed weight to a coordinate domain. """
         self._manually_weighted_domains.add(id_domain)
-        self._weights[id_domain] = weight
+        self._weights[id_domain] = np.full_like(self._energies[id_domain], weight)
 
     def unset_domain_weight(self, id_domain):
         """ Unassign fixed weight to a coordinate domain. """